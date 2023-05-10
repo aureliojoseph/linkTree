e2a005c41fe3a472d8f9d64200056d859c2c61fe
Aurelio Joseph <aurelioaborges@gmail.com>
2023-05-10 15:48:39 -0300

    updade: refactor, implement new design pattern and translate mode

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 ///////////////////////////////////////////////////////////
-// Set Current Year
+// Current Year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
-// Switch button
+// Dark/Light Mode button
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle('light')
 }
+
+///////////////////////////////////////////////////////////
+// Language Mode
+const langBtn = document.querySelector('#lang-btn');
+const translateElements = document.querySelectorAll('.translate');
+
+fetch('./translations.json')
+  .then(response => response.json())
+  .then(data => {
+    let currentLang = 'en';
+
+    langBtn.addEventListener('click', () => {
+      currentLang = currentLang === 'en' ? 'pt' : 'en';
+
+      translateElements.forEach(element => {
+        const key = element.dataset.translationKey;
+        element.textContent = data[currentLang][key];
+      });
+    });
+  });
